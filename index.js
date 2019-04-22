a564f484c9d5629814b6b8d97d4acfc3431171fa
Andy Chadha <andy.chadha2@gmail.com>
2019-04-22 17:54:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 
+function findMinAndRemoveSorted(array){
+  let min = array[0]
+  let mindex = 0
+  for(let i = 0; i < array.length; i++){
+    if(array.length === 1){
+      
+      return array[0]
+      
+    }
+    if(array[i] < min){
+      min = array[i]
+      mindex = i
+    }
+  }
+  
+  array.splice(mindex, 1)
+  return min
+}
+
+function merge(firstSubarray, secondSubArray){
+  let sorted = []
+  while(firstSubarray.length != 0 && secondSubArray.length != 0){
+    if(firstSubarray[0] < secondSubArray[0]){
+      sorted.push(findMinAndRemoveSorted(firstSubarray))
+    } else {
+      sorted.push(findMinAndRemoveSorted(secondSubArray))
+    }
+  }
+  return sorted.concat(firstSubarray).concat(secondSubArray)
+}
